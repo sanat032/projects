717c0252a2c3b20348ab39ac20f0c73bbe811c71
sanat032 <89255418+sanat032@users.noreply.github.com>
2023-06-27 15:31:11 +0530

    Update login_page.dart

--- login_page.dart
+++ login_page.dart
@@ -1,3 +1,4 @@
+//1st edit
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -14,5 +15,52 @@ class LoginPage extends StatelessWidget {
         ),
       ),
     ));
+  }
+}
+
+//2nd edit
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/undraw_secure_login_pdn4.png",
+              fit: BoxFit.cover,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Passward",
+                    labelText: "Passward",
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login"),
+                )
+              ],
+            )
+          ],
+        ));
   }
 }
