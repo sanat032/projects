0896b3921f68a307faa54d2c30381efd955d15f8
sanat032 <89255418+sanat032@users.noreply.github.com>
2023-06-28 15:36:55 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -22,4 +22,33 @@ class MyApp extends StatelessWidget {
         );
   } // MaterialApp
 }
+//3rd edit
+import 'package:flutter/material.dart';
+import 'package:tut/pages/home_page.dart';
+import 'package:tut/pages/login_page.dart';
+import 'package:tut/utils/routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.blueGrey),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        initialRoute: "/",
+        routes: {
+          "/": (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
+        }
+        // Material
+        );
+  } // MaterialApp
+}
 
