875583ddc6c18a09d6e690bec05199aee8ba1034
sanat032 <89255418+sanat032@users.noreply.github.com>
2023-07-19 14:28:57 +0530

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -1,40 +1,54 @@
+// day 13
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'package:tut/models/catalog.dart';
+import 'package:tut/widgets/drawer.dart';
+import 'package:tut/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Codepur";
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items= List.from(productsData).map<Item>((item) => Item.fromMap(item)).toList();
+ setState(() {
+   
+ });
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
       ), // AppBar
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
-        ), // Container
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: CatalogModel.items[index]);
+          },
+        ),
       ), // Center
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     ); // Scaffold
   }
 }
-//3rd edit
-import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App"),
-        backgroundColor: Colors.red,
-      ), // AppBar
-      body: Center(
-        child: Container(
-          child: Text("Welcome to my app"),
-        ), // Container
-      ), // Center
-      drawer: Drawer(),
-    ); // Scaffold
-  }
-}
