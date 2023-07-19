3485f4a9c827100ebeae5c2964a768f9e89be06d
sanat032 <89255418+sanat032@users.noreply.github.com>
2023-07-19 14:36:05 +0530

    Create catalog.dart

---
+++ catalog.dart
@@ -0,0 +1,51 @@
+class CatalogModel {
+  static  List<Item> items = [
+    Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      desc: "Apple iPhone 12th generation",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://ezone-electronics.online/wp-content/uploads/2020/12/71MHTD3uL4L._SL1500_.jpg",
+    )
+  ];
+}
+
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    return Item(
+      id: map["id"],
+      name: map["name"],
+      desc: map["desc"],
+      price: map["price"],
+      color: map["color"],
+      image: map["image"],
+    );
+  }
+  toMap() => {
+        "id": id,
+         "name": name,
+          "desc": desc,
+           "price": price,
+            "color": color,
+             "image": image,
+        
+
+      };
+}
